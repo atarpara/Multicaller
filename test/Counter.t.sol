036e371df97109be8e74f41bdad9ef23f056e588
Atarpara <akpatel0618@gmail.com>
2023-01-21 11:05:56 +0530

    inital commit

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,41 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "src/multicall.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract A {
+    function getA() public pure returns(uint256 ){
+        return 255;
+    }
 
+    function getB() public pure returns(address){
+        return address(0x12);
+    }
+}
+
+
+contract Debugger is Test{
+
+    Mutlicaller m;
+    A a;
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        m = new Mutlicaller();
+        a = new A();
     }
+    event log(bytes ll);
+    function testA() public {
+        address _target = address(a);
+        bytes[] memory returnData = new bytes[](2);
+        returnData[0] = abi.encodeWithSignature("getA()");
+        returnData[1] = abi.encodeWithSignature("getB()");
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+        Mutlicaller.Call[] memory cl = new Mutlicaller.Call[](2);
+        cl[0] = Mutlicaller.Call({target : _target, callData : returnData[0]});
+        cl[1] = Mutlicaller.Call({target : _target, callData : returnData[1]});
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        (bool s, bytes memory kk) = address(m).call(abi.encodeWithSignature("aggregate((address,bytes)[])", cl));
+        require(s);
+        emit log(kk);
     }
 }
